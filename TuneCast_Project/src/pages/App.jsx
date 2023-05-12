e4d735193d0fad6a00ec3fe10ea022591b189ca5
yurik1m <yuri.k.tech@gmail.com>
2023-05-12 10:52:25 +0900

    [23-05-12](Add) css 리셋

--- TuneCast_Project/src/pages/App.jsx
+++ TuneCast_Project/src/pages/App.jsx
@@ -1,13 +1,70 @@
 import { useState } from 'react'
+import { createGlobalStyle } from "styled-components"
 import reactLogo from '../assets/images/react.svg'
 import viteLogo from '/vite.svg'
 import '../styles/App.css'
+
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  // 추가1. 모든 태그에 border-box 적용(테두리와 안쪽 여백의 크기도 요소의 크기로 고려)
+  *{
+    box-sizing:border-box;
+  }
+  // 추가2. 링크에 모든 밑줄 삭제
+  a{
+    text-decoration : none;
+    color:inherit;
+  }
+`
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <GlobalStyle />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
