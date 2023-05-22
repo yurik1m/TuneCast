68bb95c412575e2d87bd7ed768e75b91b2d11285
Jeon Jimin <cjjunn0827@gmail.com>
2023-05-23 03:08:02 +0900

    [23-05-23](Mod) 날씨 case 추가

--- TuneCast_Project/src/styles/Gradient.js
+++ TuneCast_Project/src/styles/Gradient.js
@@ -1,13 +1,26 @@
-// 사용할 파일에서 import { gradients } from './gradients'; 로 불러와서 사용
-//
+// 사용할 파일에서 import { fetchGradient } from 'styles/gradient.js' 로 불러와서 사용
+// fetchGradient(weather) 로 사용하면 해당 날씨에 맞는 gradient를 반환
+// weather은 WeatherAPI.jsx파일의 currentWeather.weather
 
-// Thunderstorm, Drizzle, Mist, Smoke, Haze, Dust, Fog, Sand, Ash, Squall, Tornado 
+// Thunderstorm, Dust, Sand, Ash, Tornado 
+
 // Clear, Clouds, Rain, Snow, Fog
+// Rain -> Drizzle, Squall
+// Fog -> Mist, Smoke, Hase
 const gradients = {
   Clear: "linear-gradient(to bottom, #A1C4FD, #C2E9FB)",
   Cloud: "linear-gradient(to bottom, #89BCE0, #B7B7B7)",
+
   Fog: "linear-gradient(to bottom, #A8CAC8, #FF5733)",
+  Mist: "linear-gradient(to bottom, #A8CAC8, #FF5733)",
+  Smoke: "linear-gradient(to bottom, #A8CAC8, #FF5733)",
+  Haze: "linear-gradient(to bottom, #A8CAC8, #FF5733)",
+
+
   Rain: "linear-gradient(to bottom, #2B5876, #4E4376)",
+  Drizzle: "linear-gradient(to bottom, #2B5876, #4E4376)",
+  Squall: "linear-gradient(to bottom, #2B5876, #4E4376)",
+
   Snow: "linear-gradient(to bottom, #DAD4EC, #F3E7E9)"
 };
 
