c3dd099921ee6237c9e6d6414ad20b677d35d996
kyc7604 <kyc7604@gmail.com>
2023-06-16 10:01:54 +0900

    [23-06-15]Header, Footer 추가 및 일부 수정

--- TuneCast_Project/src/main.jsx
+++ TuneCast_Project/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './styles/index.css'
-
+import Playlist from './pages/Playlist.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-
+    <Playlist />
   </React.StrictMode>,
 )
